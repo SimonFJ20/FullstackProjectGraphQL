97325b965dc77a923b18d260628e329ac3eb6fcb
SimonFJ20 <simonfromjakobsen@gmail.com>
2021-07-19 21:42:02 +0200

    more

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,49 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+
 
 const port = 80;
 
 const main = async () => {
-    
+
     const orm = await MikroORM.init(mikroConfig);
     orm.getMigrator().up();
 
+    const RedisStore = connectRedis(session)
+    const redisClient = redis.createClient({
+        host: 'redis-13962.c247.eu-west-1-1.ec2.cloud.redislabs.com',
+        port: 13962,
+        password: 'yA7jCNyzosVp8PC3v2TPFrEuGbMZ6bFx'
+    });
+
+    redisClient.on('error', (error) => console.error('RedisClient Error: ' + error));
+
     const app = express();
+
+    app.use(
+        session({
+            name: 'qid',
+            store: new RedisStore({
+                client: redisClient,
+                disableTouch: true
+            }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+                httpOnly: true,
+                sameSite: 'lax',
+                secure: __prod__
+            },
+            saveUninitialized: false,
+            secret: 'qn3wgV3IUu7U8emOAdseGzwNju5R3pTD',
+            resave: false
+        })
+    );
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
@@ -26,15 +60,15 @@ const main = async () => {
             ],
             validate: false
         }),
-        context: () => ({em: orm.em})
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
     });
 
-    apolloServer.applyMiddleware({app});
+    apolloServer.applyMiddleware({ app });
 
     app.listen(port, () => {
         console.log('=== FRGTT Server on port', port, '===');
     });
-    
+
 
 }
 
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "../types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 
@@ -11,33 +11,118 @@ class UsernamePasswordInput {
     password!: string;
 }
 
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], { nullable: true })
+    errors?: FieldError[];
+
+    @Field(() => User, { nullable: true })
+    user?: User;
+}
+
 @Resolver()
 export class UserResolver {
 
-    @Mutation(() => User)
-    async register(
-        @Arg('options') {username, password}: UsernamePasswordInput,
-        @Ctx() {em}: MyContext
+    @Query(() => User, { nullable: true })
+    async me(
+        @Ctx() { em, req }: MyContext
     ) {
-        const hashedPassword = await argon2.hash(password);
-        const user = em.create(User, {username, password: hashedPassword});
-        await em.persistAndFlush(user);
+        if (!req.session.userId) {
+            return null;
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId });
         return user;
     }
 
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
+    async register(
+        @Arg('options') { username, password }: UsernamePasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        if (username.length < 3) {
+            return {
+                errors: [
+                    {
+                        field: 'username',
+                        message: 'length must be greater than 2'
+                    }
+                ]
+            }
+        }
+
+        if (password.length < 3) {
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: 'length must be greater than 2'
+                    }
+                ]
+            }
+        }
+
+        const hashedPassword = await argon2.hash(password);
+        const user = em.create(User, { username, password: hashedPassword });
+        try {
+            await em.persistAndFlush(user);
+        } catch (error: any) {
+            // || err.detail.includes('already exists')
+            if (error.code === '23505') {
+                return {
+                    errors: [
+                        {
+                            field: 'username',
+                            message: 'username already taken'
+                        }
+                    ]
+                }
+            }
+        }
+
+        req.session.userId = user.id;
+
+        return { user };
+    }
+
+    @Mutation(() => UserResponse)
     async login(
-        @Arg('options') {username, password}: UsernamePasswordInput,
-        @Ctx() {em}: MyContext
-    ) {
-        const user = await em.findOne(User, {username});
+        @Arg('options') { username, password }: UsernamePasswordInput,
+        @Ctx() { em, req }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, { username });
         if (!user) {
             return {
-                errors: [{name}]
+                errors: [{
+                    field: 'username',
+                    message: 'username doesn\'t exist'
+                }]
             }
-        }   
-        const hashedPassword = await argon2.hash(password);
-        return user;
+        }
+        const valid = await argon2.verify(user.password, password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: 'password',
+                        message: 'incorrect password'
+                    }
+                ]
+            }
+        }
+
+        req.session.userId = user.id;
+
+        return { user };
     }
 
 }
